21b284864a9f843f8834253ae428f3b714ce8491
Harshit Pande <pandeconscious@gmail.com>
2014-05-31 14:49:26 +0530

    ScoreAssigner class

---
+++ src/scorer/ScoreAssigner.java
@@ -0,0 +1,12 @@
+package scorer;
+
+import java.util.ArrayList;
+import concept.Concept;
+
+public class ScoreAssigner {
+	
+	public static Integer getScore(ArrayList<Concept> conceptList){
+		return 0;
+	}
+
+}
