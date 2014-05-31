4a53e8373e74f98e04f2fa268f2bb63b2254489b
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-05-31 13:11:47 +0530

    Add sentence class

---
+++ src/delic/Sentence.java
@@ -0,0 +1,18 @@
+package delic;
+
+public class Sentence {
+	String sentenceStr;
+
+	public Sentence(String sentenceStr) {
+		this.sentenceStr = sentenceStr;
+	}
+
+	public void setSentenceStr(String sentenceStr) {
+		this.sentenceStr = sentenceStr;
+	}
+
+	public String getSentenceStr() {
+		return sentenceStr;
+	}
+
+}
