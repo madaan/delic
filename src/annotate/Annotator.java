84328e1820ee6636c332a5efcb15bfbf93ab2a22
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-05-31 14:21:19 +0530

    Add annotator

---
+++ src/annotate/Annotator.java
@@ -0,0 +1,15 @@
+package annotate;
+
+/**
+ * This is the workhorse, responsible for annotating the 
+ * given document. Gets a document and returns an arraylist of
+ * @author aman
+ *
+ */
+public class Annotator {
+
+	public Annotator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
